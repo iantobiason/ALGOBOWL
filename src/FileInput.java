0d91a89ff12b0475c94764417fea3f597d3dbdc2
Ian Tobiason <itobiaso@mines.edu>
2017-02-26 13:41:56 -0700

    text files

--- src/FileInput.java
+++ src/FileInput.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FileInput {
-	String File = "test.txt";
+	String File = "bad.txt";
 	int numberOfTasks;
 	int numberOfMachines;
 	ArrayList<Integer> tasks = new ArrayList<Integer>();
