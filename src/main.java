a49057f2192a06ecd2875967df5b6149d988a2f5
Ian Tobiason <itobiaso@mines.edu>
2017-02-26 12:03:28 -0700

    File input class

--- src/main.java
+++ src/main.java
@@ -1,8 +1,25 @@
-
 public class main {
 
 	public main() {
 		// TODO Auto-generated constructor stub
 	}
 
+
+	public static void main(String [] args) {
+		FileInput input = new FileInput();
+		input.startUp();
+		System.out.println(input.numberOfTasks);
+		System.out.println(input.numberOfMachines);
+		for(int i = 0; i < input.tasks.size(); i++) {
+			System.out.print(input.tasks.get(i));
+			System.out.print(" ");
+			
+		}
+		System.out.println();
+		for(int i = 0; i < input.machines.size(); i++) {
+			System.out.print(input.machines.get(i));
+			System.out.print(" ");
+		}
+		
+	}
 }
